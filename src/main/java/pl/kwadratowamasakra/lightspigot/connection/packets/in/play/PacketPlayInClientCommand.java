76e3ccae3c6d68f0b8739b0e0e6d6f293f40cafb
radek203 <r.porebinski@gmail.com>
2025-05-31 22:03:17 +0200

    Add support for 1.11-1.11.2

--- src/main/java/pl/kwadratowamasakra/lightspigot/connection/packets/in/play/PacketPlayInClientCommand.java
+++ src/main/java/pl/kwadratowamasakra/lightspigot/connection/packets/in/play/PacketPlayInClientCommand.java
@@ -28,6 +28,6 @@ public class PacketPlayInClientCommand extends PacketIn {
     public enum EnumState {
         PERFORM_RESPAWN,
         REQUEST_STATS,
-        OPEN_INVENTORY_ACHIEVEMENT //1.8.x,1.9.x,1.10.x
+        OPEN_INVENTORY_ACHIEVEMENT //1.8.x,1.9.x,1.10.x,1.11.x
     }
 }
